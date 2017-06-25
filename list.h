f2fe0cf13f47250f99095d9625d59f89e271bb8f
bfady33 <bfady33@gmail.com>
2017-06-25 15:59:59 +0300

    list.h 1.0

---
+++ list.h
@@ -0,0 +1,17 @@
+//
+// Created by Fady on 6/25/2017.
+//
+
+#ifndef HW4PROJECT_LIST_H
+#define HW4PROJECT_LIST_H
+
+template <class T>
+class List {
+public:
+
+
+private:
+
+};
+
+#endif //HW4PROJECT_LIST_H
